150f9ef48573f320c65a4d92caf457a1bda23fcf
unknown <ganorkarmohit551@gmail.com>
2026-01-21 17:14:47 +0530

    Code Modified : Model is Done

---
+++ Package/Model/Model.go
@@ -0,0 +1,37 @@
+package Model
+
+import Util "github.com/FACELESS-GOD/RAFTLogStore/Package/Utility"
+
+type ModelInterFace interface {
+	AddLog(LogStuct) (bool, error)
+	GetLog(int) (LogStuct, error)
+}
+
+type LogStuct struct {
+	Text string
+}
+
+type ModelStuct struct {
+	Utility Util.UtilStruct
+	Arr     []string
+}
+
+func NewModel(UT Util.UtilStruct) (ModelStuct, error) {
+	arr := make([]string, 10, 10)
+	mdl := ModelStuct{Utility: UT, Arr: arr}
+
+	return mdl, nil
+}
+
+func (Mdl *ModelStuct) AddLog(LogStuct LogStuct) (bool, error) {
+	Mdl.Arr = append(Mdl.Arr, LogStuct.Text)
+	return true, nil
+}
+
+func (Mdl *ModelStuct) GetLog(Id int) (LogStuct, error) {
+	log := LogStuct{}
+	if Id < len(Mdl.Arr) {
+		log.Text = Mdl.Arr[Id]
+	}
+	return log, nil
+}
